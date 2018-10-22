22040bf887b08556f705df575fd65ea1a3b480c6
Zhiji Gu <zhiji_gu@feiliks.com>
2018-10-22 14:39:06 +0800

    merge new js impl & disk io feature

--- src/main/java/com/feiliks/dashboard/spring/BackgroundTask.java
+++ src/main/java/com/feiliks/dashboard/spring/BackgroundTask.java
@@ -16,7 +16,9 @@ public class BackgroundTask {
 
     private Map<String, Long[]> lastCpuTime = null;
     private Map<String, Long[]> lastRxTx = null;
-    private long lastRxTxTime;
+    private double lastRxTxTime;
+    private Map<String, Long[]> lastIO = null;
+    private double lastDiskIOTime;
 
 
     @Scheduled(fixedDelay = 2000)
@@ -50,24 +52,47 @@ public class BackgroundTask {
     @Scheduled(fixedDelay = 2000)
     public void pushNetworkRxTx() {
         Map<String, Long[]> ifaces = SysInfo.getIfaces();
-        long curTime = System.currentTimeMillis() / 1000;
+        double curTime = System.currentTimeMillis() / 1000;
         if (ifaces != null && lastRxTx != null && curTime > lastRxTxTime) {
             StringBuilder out = new StringBuilder("net:");
-            long timeDiff = curTime - lastRxTxTime;
+            double timeDiff = curTime - lastRxTxTime;
             for (Map.Entry<String, Long[]> iface : ifaces.entrySet()) {
                 Long[] prev = lastRxTx.get(iface.getKey());
                 Long[] cur = iface.getValue();
                 out.append(iface.getKey())
                         .append('=')
-                        .append((cur[0] > prev[0]) ? (1.0 * (cur[0] - prev[0]) / timeDiff) : 0.0)
+                        .append((cur[0] > prev[0]) ? ((cur[0] - prev[0]) / timeDiff) : 0.0)
                         .append(',')
-                        .append((cur[1] > prev[1]) ? (1.0 * (cur[1] - prev[1]) / timeDiff) : 0.0)
+                        .append((cur[1] > prev[1]) ? ((cur[1] - prev[1]) / timeDiff) : 0.0)
                         .append(';');
             }
             webSocketHandler.broadcast(out.toString());
         }
         lastRxTx = ifaces;
         lastRxTxTime = curTime;
+    }
+
+    @Scheduled(fixedDelay = 2000)
+    public void pushDiskIO() {
+        Map<String, Long[]> disks = SysInfo.getDiskIO();
+        double curTime = System.currentTimeMillis() / 1000.0;
+        if (disks != null && lastIO != null && curTime > lastDiskIOTime) {
+            StringBuilder out = new StringBuilder("disk:");
+            double timeDiff = curTime - lastDiskIOTime;
+            for (Map.Entry<String, Long[]> disk : disks.entrySet()) {
+                Long[] prev = lastIO.get(disk.getKey());
+                Long[] cur = disk.getValue();
+                out.append(disk.getKey())
+                        .append('=')
+                        .append((cur[0] > prev[0]) ? ((cur[0] - prev[0]) / timeDiff) : 0.0)
+                        .append(',')
+                        .append((cur[1] > prev[1]) ? ((cur[1] - prev[1]) / timeDiff) : 0.0)
+                        .append(';');
+            }
+            webSocketHandler.broadcast(out.toString());
+        }
+        lastIO = disks;
+        lastDiskIOTime = curTime;
     }
 
 }
