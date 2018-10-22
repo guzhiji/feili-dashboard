66685bfa51e3909c5d5c6d1b9208f312d4415bf4
Zhiji Gu <zhiji_gu@feiliks.com>
2018-10-22 10:01:10 +0800

    improve NetMonitor accuracy: use double for seconds

--- src/main/java/com/feiliks/dashboard/javax/NetMonitor.java
+++ src/main/java/com/feiliks/dashboard/javax/NetMonitor.java
@@ -5,23 +5,23 @@ import java.util.Map;
 
 public class NetMonitor extends DashboardServer.DashboardMonitorTask {
     private Map<String, Long[]> lastRxTx = null;
-    private long lastRxTxTime;
+    private double lastRxTxTime;
 
     @Override
     public void run() {
         Map<String, Long[]> ifaces = SysInfo.getIfaces();
-        long curTime = System.currentTimeMillis() / 1000;
+        double curTime = System.currentTimeMillis() / 1000;
         if (ifaces != null && lastRxTx != null && curTime > lastRxTxTime) {
             StringBuilder out = new StringBuilder();
-            long timeDiff = curTime - lastRxTxTime;
+            double timeDiff = curTime - lastRxTxTime;
             for (Map.Entry<String, Long[]> iface : ifaces.entrySet()) {
                 Long[] prev = lastRxTx.get(iface.getKey());
                 Long[] cur = iface.getValue();
                 out.append(iface.getKey())
                         .append('=')
-                        .append((cur[0] > prev[0]) ? (1.0 * (cur[0] - prev[0]) / timeDiff) : 0.0)
+                        .append((cur[0] > prev[0]) ? ((cur[0] - prev[0]) / timeDiff) : 0.0)
                         .append(',')
-                        .append((cur[1] > prev[1]) ? (1.0 * (cur[1] - prev[1]) / timeDiff) : 0.0)
+                        .append((cur[1] > prev[1]) ? ((cur[1] - prev[1]) / timeDiff) : 0.0)
                         .append(';');
             }
             broadcast("net", out.toString());
