0ce45f515c39baa9d76a8281901846d38dedc1f5
Zhiji Gu <zhiji_gu@feiliks.com>
2019-01-16 16:33:56 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function() {
+	return gulp.src('./sass/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('./src/main/resources/static'));
+});
+
+gulp.task('sass:watch', function() {
+	glup.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('js', function() {
+	return gulp.src('./js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./src/main/resources/static'));
+});
+
